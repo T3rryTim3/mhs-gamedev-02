49a05a7b1ec2045ce84d0164a0463815cec9be5f
479619 <479619@bsd48.org>
2024-12-16 13:53:31 -0800

    began settings menu

--- projects/title-screen/main_menu.gd
+++ projects/title-screen/main_menu.gd
@@ -1,9 +1,11 @@
 extends Control
 
-# Declare button variables
-var button1 : Button
-var button2 : Button
 
+@onready var menu = $smenu
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_2_pressed() -> void:
+	pass
