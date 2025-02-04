8a5e09d5c34f7135aa448a14df04b1c8b5bb481a
479619 <479619@bsd48.org>
2025-02-04 12:35:39 -0800

    started coding the screen

--- projects/mhsdev-02/blueprint_screen.gd
+++ projects/mhsdev-02/blueprint_screen.gd
@@ -7,19 +7,5 @@ extends PanelContainer
 func _ready():
 	bprint_menu.visible = false
 
-func _set_menu(menu):
-	var wasClosed = menu.visible == false
-	
-	_close_all_menus()
-	
-	bprint_menu.visible = wasClosed
-	menu.visible = wasClosed
-
-func _close_all_menus():
-	for menu in intern_menu.get_children():
-		menu.visible = false
-
-
-
 func _on_button_pressed() -> void:
-	_set_menu(bluprints)
+	bprint_menu.visible = not bprint_menu.visible
