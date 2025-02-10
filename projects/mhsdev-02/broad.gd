b35c57af1e3f2c7c80a476ccc3e20952be181b31
479619 <479619@bsd48.org>
2025-02-10 13:53:07 -0800

    Added loading for hover blueprint and cost

--- projects/mhsdev-02/broad.gd
+++ projects/mhsdev-02/broad.gd
@@ -1,13 +1,19 @@
 extends PanelContainer
 
+@export var target_station:StationData.Stations
 
-
-
+@onready var texture = $TextureRect
 
 func _on_mouse_entered() -> void:
-	Popuppanel.ItemPopup(null,null)
+	Popuppanel.ItemPopup(Rect2i( Vector2i(global_position) , Vector2i(size)),self)
 
-
+	print("show" + str(randf()))
 
 func _on_mouse_exited() -> void:
-	Popuppanel.HideItemPopup(null,null)
+	Popuppanel.HideItemPopup()
+	print("Hidden")
+
+func _ready() -> void:
+	var image = load(StationData.get_station_texture(target_station))
+	texture.texture = image
+	
