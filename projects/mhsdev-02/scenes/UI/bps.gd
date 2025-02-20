29872757926bba5587e9caace186c0ce5d79d261
479619 <479619@bsd48.org>
2025-02-20 13:53:09 -0800

    began new blueprint system

---
+++ projects/mhsdev-02/scenes/UI/bps.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var bprint_menu = $MarginContainer/HBoxContainer/Panel
+@onready var intern_menu = $MarginContainer/HBoxContainer/Panel/MarginContainer
+@onready var bluprints = $MarginContainer/HBoxContainer/Panel/MarginContainer/GridContainer
+
+func _ready():
+	bprint_menu.visible = false
+
+func _on_button_pressed() -> void:
+	bprint_menu.visible = not bprint_menu.visible
