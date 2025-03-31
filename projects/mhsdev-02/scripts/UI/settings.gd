5bd390c081a41d0d44d4d3169eecaf8e45cfc379
479619 <479619@bsd48.org>
2025-03-31 12:23:52 -0700

    Update settings.gd

--- projects/mhsdev-02/scripts/UI/settings.gd
+++ projects/mhsdev-02/scripts/UI/settings.gd
@@ -10,10 +10,10 @@ func _ready():
 	exit_button.pressed.connect(close_menu)
 
 func _on_master_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/SoundMenu/MarginContainer/VBoxContainer/Master.value))
+	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/Master.value))
 
 func _on_music_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/SoundMenu/MarginContainer/VBoxContainer/Music.value))
+	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/Music.value))
 
 func _on_sfx_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/SoundMenu/MarginContainer/VBoxContainer/SFX.value))
+	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/SFX.value))
