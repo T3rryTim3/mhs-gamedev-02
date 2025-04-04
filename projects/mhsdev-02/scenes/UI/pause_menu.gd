5b43320a51fefddb52717862614e50e70e8b7c54
479619 <479619@bsd48.org>
2025-04-04 16:39:22 -0700

    bugfixes

--- projects/mhsdev-02/scenes/UI/pause_menu.gd
+++ projects/mhsdev-02/scenes/UI/pause_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var resume = $MarginContainer/VBoxContainer/Resume
 @onready var quit = $MarginContainer/VBoxContainer/Quit
 @onready var menu = %Settings
+@onready var title = %Titlescreen
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("pause"):
@@ -23,4 +24,5 @@ func _on_settings_pressed() -> void:
 	menu.visible = true
 
 func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/UI/MainMenu.tscn")
+	title.visible = true
+	visible = false
--- projects/mhsdev-02/scripts/UI/settings.gd
+++ projects/mhsdev-02/scripts/UI/settings.gd
@@ -11,19 +11,18 @@ func _ready():
 	exit_button.pressed.connect(close_menu)
 	keybinds.visible = false
 	misc.visible = false
+	_on_master_value_changed()
+	_on_sfx_value_changed()
+	_on_music_value_changed()
 
+func _on_master_value_changed(_value: float = 1) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/Master.value/1000))
 
+func _on_music_value_changed(_value: float = 1) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/Music.value/1000))
 
-func _on_master_value_changed(value: float) -> void:
-	print("Changed")
-	AudioServer.set_bus_volume_db(0, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/Master.value))
-
-func _on_music_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(1, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/Music.value))
-	print(AudioServer.get_bus_volume_db(0))
-
-func _on_sfx_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(2, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/SFX.value))
+func _on_sfx_value_changed(_value: float = 1) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/SFX.value/1000))
 
 
 func _on_keybinds_button_pressed() -> void:
