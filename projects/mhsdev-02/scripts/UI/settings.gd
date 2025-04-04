c00a5ca1c3173776d17499081397f5cc756e0728
479619 <479619@bsd48.org>
2025-04-04 13:27:02 -0700

    general changes

--- projects/mhsdev-02/scripts/UI/settings.gd
+++ projects/mhsdev-02/scripts/UI/settings.gd
@@ -1,12 +1,17 @@
 extends MarginContainer
 
 @onready var exit_button = $ExitButton
+@onready var keybinds = $TabContainer/Game/Keybinds
+@onready var misc = $TabContainer/Game/control
 
 func close_menu():
 	visible = false
 
 func _ready():
 	exit_button.pressed.connect(close_menu)
+	keybinds.visible = false
+	misc.visible = false
+
 
 
 func _on_master_value_changed(value: float) -> void:
@@ -19,3 +24,13 @@ func _on_music_value_changed(value: float) -> void:
 
 func _on_sfx_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(2, linear_to_db($TabContainer/Sound/MarginContainer/VBoxContainer/SFX.value))
+
+
+func _on_keybinds_button_pressed() -> void:
+	keybinds.visible = true
+	misc.visible = false
+
+func _on_gamemode_button_pressed() -> void:
+	misc.visible = true
+	keybinds.visible = false
+	
