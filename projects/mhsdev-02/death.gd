6c9db9b348d4a080c34c0c277e760788adda7d6d
479619 <479619@bsd48.org>
2025-04-13 23:20:59 -0700

    Created Death Screen

---
+++ projects/mhsdev-02/death.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var you_died = $MarginContainer/AnimatedSprite2D
+@onready var title = %Titlescreen
+
+func _ready() -> void:
+	visible = true
+	
+
+
+
+func _on_quit_pressed() -> void:
+	visible = false
+	title.visible = true
+	
+	
