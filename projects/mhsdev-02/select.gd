0a35482c395d3284c2964386078bc83ea37599d9
479619 <479619@bsd48.org>
2025-04-14 15:01:09 -0700

    created more of the level selection

--- projects/mhsdev-02/select.gd
+++ projects/mhsdev-02/select.gd
@@ -3,14 +3,40 @@ extends Control
 var levels = [
 	{
 		"name": "Field",
-		"image_path": ""
+		"node_path": "res://scenes/Levels/field.tscn"
 	},
-	{
-		"name": "Tutorial",
-		"image_path": ""
-	}
+	#{
+		#"name": "Tutorial",
+		#"node_path": "tutorial"
+	#}
 ]
 
 
-func _ready() -> void:
+func _ready():
+	var buttons = [
+		$TextureButton,
+		$TextureButton2
+	]
+	
+	for i in range(levels.size()):
+		var data = levels[i]
+		var button = buttons[i]
+		
+		var tex = load(data["node_path"])
+		if tex:
+			button.texture_normal = tex
+		
+		button.set("level_data", data)
+		
+		button.pressed.connect(_on_LevelButton_pressed.bind(data))
+
+func _on_LevelButton_pressed(level_data):
+	visible = false
+	
+
+func _on_button_pressed() -> void:
+	pass 
+	
+
+func _on_button_2_pressed() -> void:
 	pass
