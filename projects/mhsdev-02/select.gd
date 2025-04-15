112d1652db3d77ded978ee1594384b436318cc7b
479619 <479619@bsd48.org>
2025-04-15 11:15:18 -0700

    fixed buttons

--- projects/mhsdev-02/select.gd
+++ projects/mhsdev-02/select.gd
@@ -4,17 +4,17 @@ var levels = [
 	{
 		"name": "Field",
 		"node_path": "res://scenes/Levels/field.tscn",
-		"image_path": ""
+		"image_path": "res://images/items/wheat.png"
 	},
 	
 	{
 		"name": "Tutorial",
 		"node_path": "res://scenes/Levels/tutorial.tscn",
-		"image_path": ""
+		"image_path": "res://images/items/bread.png"
 	}
 ]
 
-var current_level_index = 1
+var current_level_index = 0
 
 func update_button(button):
 	var data = levels[current_level_index]
@@ -36,8 +36,8 @@ func _ready():
 
 func _on_Forward_pressed():
 	current_level_index += 1
-	if current_level_index == levels.size():
-		current_level_index = 1 
+	if current_level_index >= levels.size():
+		current_level_index = 0 
 	update_button($TextureButton)
 
 func _on_Back_pressed():
