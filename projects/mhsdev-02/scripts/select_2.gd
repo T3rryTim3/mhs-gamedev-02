1d86d5ed95dc91ff452cac1ee54742df20e2bc6a
479619 <479619@bsd48.org>
2025-05-07 22:28:09 -0700

    Background Colours

--- projects/mhsdev-02/scripts/select_2.gd
+++ projects/mhsdev-02/scripts/select_2.gd
@@ -14,13 +14,15 @@ var all_levels = [
 		"name": "Tutorial",
 		"description": "A guide on how to use (parentheses)",
 		"color": Color(0.9,0.9,0.9),
-		"bg_color":Color(1,1,1),
+		"bg_color":Color(0.922,0.962,0.387),
 		"scene_enum": Main.Scenes.LEVEL_TUTORIAL,
 		"image_path": "res://images/placeholder/image.png",
 		"modes": [
 			{
 				"name": "Tutorial",
 				"leveldata": Config.GameDifficulties.TUTORIAL,
+				"color": Color(0.9,0.9,0.9),
+				"bg_color":Color(0.271,0.0,0.111),
 				"desc": [
 					["None", Color(0.4,1,0.4)]
 				]
@@ -31,7 +33,7 @@ var all_levels = [
 		"name": "Field",
 		"description": "An empty field perfect for not having (parentheses)",
 		"color": Color(0,1,0),
-		"bg_color":Color(1,1,1),
+		"bg_color":Color(0,0.504,0.083),
 		"scene_enum": Main.Scenes.LEVEL_FIELD,
 		"image_path": "res://images/placeholder/image.png",
 		"modes": [
@@ -39,7 +41,7 @@ var all_levels = [
 				"name": "Standard",
 				"leveldata": Config.GameDifficulties.FIELD_STANDARD,
 				"color" : Color(0.4, 1, 0.4),
-				"bg_color":Color(1,1,1),
+				"bg_color":Color(0.252,0.559,0.0),
 				"desc": [
 					["55s Event delay", Color(1,0,0)],
 					["-0.4 Temperature", Color(1,0,0)]
@@ -51,6 +53,7 @@ var all_levels = [
 				"achievement": Achievements.Achievements.FIELD_STANDARD,
 				"leveldata": Config.GameDifficulties.FIELD_ROWDY,
 				"color" : Color(1, 1, 0.4),
+				"bg_color":Color(0.873, 0.896, 0.0),
 				"desc": [
 					["-10s between events", Color(1,0,0)],
 					["Hunger decreases faster", Color(1,0,0)],
@@ -62,6 +65,7 @@ var all_levels = [
 				"name": "Mayhem",
 				"achievement": Achievements.Achievements.FIELD_ROWDY,
 				"color" : Color(1, 0.4, 0.4),
+				"bg_color":Color(0.878, 0.0, 0.282),
 				"leveldata": Config.GameDifficulties.FIELD_MAYHEM,
 				"desc": [
 					["All events are doubled", Color(1,0,0)],
@@ -74,6 +78,7 @@ var all_levels = [
 		"name": "Tundra",
 		"description": "A harsh environment requiring a good source of heat and food.",
 		"color": Color(1,1,1),
+		"bg_color":Color(0.0, 0.573, 0.655),
 		"scene_enum": Main.Scenes.LEVEL_TUNDRA,
 		"achievement": Achievements.Achievements.FIELD_STANDARD,
 		"image_path": "res://images/placeholder/Screenshot from 2025-05-01 20-43-53.png",
@@ -81,6 +86,8 @@ var all_levels = [
 			{
 				"name": "Standard",
 				"leveldata": Config.GameDifficulties.TUNDRA_STANDARD,
+				"color": Color(1,1,1),
+				"bg_color":Color(0.52, 1.0, 0.914),
 				"desc": [
 					["60s Event delay", Color(1,0,0)],
 					["-1.2 Temperature", Color(1,0,0)],
@@ -92,6 +99,8 @@ var all_levels = [
 			{
 				"name": "Rowdy",
 				"leveldata": Config.GameDifficulties.TUNDRA_ROWDY,
+				"color": Color(1,1,1),
+				"bg_color":Color(0.697, 0.001, 0.775),
 				"desc": [
 					["-10s between events", Color(1,0,0)],
 					["Hunger decreases faster", Color(1,0,0)],
@@ -102,6 +111,8 @@ var all_levels = [
 			{
 				"name": "Mayhem",
 				"leveldata": Config.GameDifficulties.TUNDRA_MAYHEM,
+				"color": Color(1,1,1),
+				"bg_color":Color(0.756, 0.0, 0.433),
 				"desc": [
 					["All events are doubled", Color(1,0,0)],
 					["Temperature drains twice as fast", Color(1,0,0)]
@@ -113,6 +124,7 @@ var all_levels = [
 		"name": "Desert",
 		"description": "A harsh environment requiring a good source of water.",
 		"color": Color(1,0.9,0),
+		"bg_color":Color(0.821, 0.73, 0.0),
 		"scene_enum": Main.Scenes.LEVEL_TUNDRA,
 		"achievement": Achievements.Achievements.FIELD_STANDARD,
 		"image_path": "res://images/placeholder/Screenshot from 2025-05-01 20-43-53.png",
@@ -120,6 +132,8 @@ var all_levels = [
 			{
 				"name": "Standard",
 				"leveldata": Config.GameDifficulties.TUNDRA_STANDARD,
+				"color": Color(1,0.9,0),
+				"bg_color":Color(0.074, 0.815, 1.0),
 				"desc": [
 					["60s Event delay", Color(1,0,0)],
 					["-1.2 Temperature", Color(1,0,0)],
@@ -131,6 +145,8 @@ var all_levels = [
 			{
 				"name": "Rowdy",
 				"leveldata": Config.GameDifficulties.TUNDRA_ROWDY,
+				"color": Color(1,0.9,0),
+				"bg_color":Color(0.367, 0.175, 0.0),
 				"desc": [
 					["-10s between events", Color(1,0,0)],
 					["Hunger decreases faster", Color(1,0,0)],
@@ -141,6 +157,8 @@ var all_levels = [
 			{
 				"name": "Mayhem",
 				"leveldata": Config.GameDifficulties.TUNDRA_MAYHEM,
+				"color": Color(1,0.9,0),
+				"bg_color":Color(0.986, 0.0, 0.558),
 				"desc": [
 					["All events are doubled", Color(1,0,0)],
 					["Temperature drains twice as fast", Color(1,0,0)]
@@ -152,12 +170,15 @@ var all_levels = [
 		"name": "Custom",
 		"description": "A sandbox to play as you wish",
 		"color": Color(1,0,1),
+		"bg_color":Color(0.306, 0.001, 0.455),
 		"scene_enum": "p",
 		"image_path": "res://images/placeholder/image.png",
 		"modes": [
 			{
-				"name": "Tutorial",
+				"name": "Custom",
 				"leveldata": Config.GameDifficulties.TUTORIAL,
+				"color": Color(1,0,1),
+				"bg_color":Color(0.986, 0.0, 0.558),
 				"desc": [
 					["x2 Events", Color(1,0,0)],
 					["x2 Events", Color(1,0,0)]
